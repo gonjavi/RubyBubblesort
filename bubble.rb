5fc943880a3c9958cf9496b4a312adea933dcad2
Gabriela Cruz <viridiana.cruz.olivera@gmail.com>
2019-07-16 13:17:10 -0700

    refactored bubble_sort method

--- bubble.rb
+++ bubble.rb
@@ -1,22 +1,15 @@
  def bubble_sort(arr)
-
-  for i in 0...arr.length
-    for i in 0...arr.length-1 
-      if arr[i] > arr[i+1]
-        left = arr[i]
-        arr[i] = arr[i+1]
-        arr[i+1] = left
-      end
+  n = arr.length
+  for i in 0...n
+    for j in 0...n - 1  
+      left, right = arr[j], arr[j + 1]     
+      arr[j], arr[j + 1] = right, left if left > right
     end
   end
   
   arr
  end
 
- #puts "input the array"
- #x=gets.chomp
-
- #puts x
-
  arr = [4,3,78,2,0,2]
  print bubble_sort(arr)
+
