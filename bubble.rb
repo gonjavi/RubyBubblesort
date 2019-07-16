71f2610d7ed32890a2b21cc8cc74a3769aa354c7
Gabriela Cruz <viridiana.cruz.olivera@gmail.com>
2019-07-16 09:51:56 -0700

    bubble sort method finished

--- bubble.rb
+++ bubble.rb
@@ -1,8 +1,22 @@
- def bubble_sort
+ def bubble_sort(arr)
 
+  for i in 0...arr.length
+    for i in 0...arr.length-1 
+      if arr[i] > arr[i+1]
+        left = arr[i]
+        arr[i] = arr[i+1]
+        arr[i+1] = left
+      end
+    end
+  end
+  
+  arr
  end
 
- puts "input the array"
- x=gets.chomp
+ #puts "input the array"
+ #x=gets.chomp
 
- puts x
+ #puts x
+
+ arr = [4,3,78,2,0,2]
+ print bubble_sort(arr)
